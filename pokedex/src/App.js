d47447a0b8789531e018ed86cdf67441944b378b
Monalisa_Kairine <Monakairine@users.noreply.github.com>
2021-03-29 22:26:59 -0300

    coloquei o theme em um arquivo separado

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,38 +1,15 @@
 import React from 'react'
 import Router from './routes/Router'
 import GlobalState from './global/GlobalState'
-import {ThemeProvider, createMuiTheme} from '@material-ui/core';
-
+import {ThemeProvider} from '@material-ui/core';
+import theme from './constants/theme'
 
 function App() {
-
-  const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#f44336',
-      }
-    },
-
-    typography: {
-      h3:{
-        "fontWeight": 700,
-      },
-      h5: {
-        "fontWeight": 700,
-        "padding": 4,
-      },
-      h6:{
-        "padding": 4,
-      },
-    },
-
-  });
-
 
   return (
     <GlobalState>
       <ThemeProvider theme={theme}>
-      <Router/>
+        <Router/>
       </ThemeProvider>
     </GlobalState>
   );
